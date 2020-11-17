f32284f4d80a01d87cd620a17dd3e05dd45398da
da24537 <1891005016aw@cyber-u.ac.jp>
2020-11-17 17:28:30 +0000

    resubmit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,23 @@
 class TasksController < ApplicationController
 
   def index
-    @tasks = Task.all
+    @task = Task.all
   end
 
   def show
-    @tasks = Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def new
-    @tasks = Task.new
+    @task = Task.new
   end
 
   def create
-    @tasks = Task.new(task_params)
+    @task = Task.new(task_params)
 
-    if @tasks.save
+    if @task.save
       flash[:success] = 'タスク が正常に投稿されました'
-      redirect_to @tasks
+      redirect_to @task
     else
       flash.now[:danger] = 'タスク が投稿されませんでした'
       render :new
@@ -25,15 +25,15 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @tasks = Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def update
-    @tasks = Task.find(params[:id])
+    @task = Task.find(params[:id])
 
-    if @tasks.update(task_params)
+    if @task.update(task_params)
       flash[:success] = 'タスク は正常に更新されました'
-      redirect_to @tasks
+      redirect_to @task
     else
       flash.now[:danger] = 'タスク は更新されませんでした'
       render :edit
@@ -41,8 +41,8 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @tasks = Task.find(params[:id])
-    @tasks.destroy
+    @task = Task.find(params[:id])
+    @task.destroy
 
     flash[:success] = 'タスク は正常に削除されました'
     redirect_to tasks_url
--- app/models/kadai_tasklisy.rb
+++
@@ -1,2 +0,0 @@
-class KadaiTasklisy < ApplicationRecord
-end
