dccf2dfa6e7a13d3d1bd4f706461e14da8931750
da24537 <1891005016aw@cyber-u.ac.jp>
2020-11-19 01:40:08 +0000

    rex2-submit

--- db/migrate/20201112062624_create_kadai_tasklisies.rb
+++
@@ -1,9 +0,0 @@
-class CreateKadaiTasklisies < ActiveRecord::Migration[5.2]
-  def change
-    create_table :kadai_tasklisies do |t|
-      t.string :content
-
-      t.timestamps
-    end
-  end
-end
